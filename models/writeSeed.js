3ddabde527e188e65b6262e116cfb06ee8e611b6
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-06-02 18:22:21 -0400

    removed some css

--- models/writeSeed.js
+++ models/writeSeed.js
@@ -3,21 +3,21 @@ module.exports = [
         name: 'Real Madrid',
         description: 'Home Real Madrid Jersey.',
         img:'https://www.amstadion.com/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/r/m/rm-kjh02101-2_4.jpg',
-        price: 70,
+        price: 90,
         qty:100
     },
     {
         name: 'Chelsea FC',
         description: 'Home Chelsea FC Jersey.',
         img:'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/b62dbf5d-11ee-4c63-995f-c80630157cd4/chelsea-fc-2021-22-stadium-home-mens-soccer-jersey-3BlHlg.png',
-        price: 70,
+        price: 60,
         qty:100
     },
     {
         name: 'Inter Milan',
         description: 'Home Inter Milan Jersey.',
         img:'https://static1.cdn-subsidesports.com/2/media/catalog/product/cache/8c1d2c81075bec58441bdd78446b18bb/9/0/90b97ef5f592f1e2e7021ee0628040ce46bb2d4feca5dd08abb2cb7ecc1baa9f.jpeg',
-        price: 70,
+        price: 100,
         qty:100
     },
     {
@@ -31,14 +31,14 @@ module.exports = [
         name: 'Manchester United',
         description: 'Cristiano Ronaldo Home Manchester United Jersey.',
         img:'https://images.footballfanatics.com/manchester-united/manchester-united-cup-home-shirt-2021-22-with-ronaldo-7-printing_ss4_p-12093052+u-1ep8mddmd2rktpu3u302+v-149c3800d6d049d1a7f7e375c48d3860.jpg?_hv=1&w=600',
-        price: 70,
+        price: 120,
         qty:100
     },
     {
         name: 'Juventus',
         description: 'Home Juventus Jersey.',
         img:'https://store.juventus.com/data/store/product/4/47060/product.jpg',
-        price: 70,
+        price: 50,
         qty:100
     }
     
