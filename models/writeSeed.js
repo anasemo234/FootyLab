49646c1adb3024ea0dab55b7c9949301fc0f0132
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-06-02 22:35:39 -0400

    wip

--- models/writeSeed.js
+++ models/writeSeed.js
@@ -29,14 +29,14 @@ module.exports = [
     },
     {
         name: 'Manchester United',
-        description: 'Barcelona – as they do – go where no club has gone before. The “purple pulse” color palette plays host to an iridescent club and Nike crest on the chest. Yes, a shiny, reflective badge application on a professional soccer kit. Nike didn’t hold back. The jersey is meant to be a kit for everyone, one driving unity and togetherness, because, after all – tots units fem forca.',
+        description: 'This is the United state of mind. The heart of Manchester is beating red, and the people there remember – they lived through – the greats who wore that red. George Best, Bobby Charlton and the like. This kit hearkens back to that era, that grit, that resilience, the values that built the city. This classy home jersey will connect you to that attitude, to the city of Manchester and the people that built it.',
         img:'https://images.footballfanatics.com/manchester-united/manchester-united-cup-home-shirt-2021-22-with-ronaldo-7-printing_ss4_p-12093052+u-1ep8mddmd2rktpu3u302+v-149c3800d6d049d1a7f7e375c48d3860.jpg?_hv=1&w=600',
         price: 120,
         qty:100
     },
     {
         name: 'Juventus',
-        description: 'Barcelona – as they do – go where no club has gone before. The “purple pulse” color palette plays host to an iridescent club and Nike crest on the chest. Yes, a shiny, reflective badge application on a professional soccer kit. Nike didn’t hold back. The jersey is meant to be a kit for everyone, one driving unity and togetherness, because, after all – tots units fem forca.',
+        description: 'A return to the classic Juve design marks the 10th anniversary of the Allianz Stadium – a home pitch where Juventus have constantly reinvented themselves, moved forward, trailblazed as a club. But also, a place where their history lies, where countless moments are etched into the memories of supporters and players alike. And those memories? Likely in a kit that looked just like this one. Here’s to a return to tradition, as the classic stripes return unaltered, here’s a look towards the future, as Juventus reinvent again and again.',
         img:'https://store.juventus.com/data/store/product/4/47060/product.jpg',
         price: 50,
         qty:100
