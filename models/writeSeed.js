dfc13f0454d368cd06face89b9fe6e550fece44b
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-06-02 20:32:45 -0400

    wip

--- models/writeSeed.js
+++ models/writeSeed.js
@@ -1,42 +1,42 @@
 module.exports = [
     {
         name: 'Real Madrid',
-        description: 'Home Real Madrid Jersey.',
+        description: 'In the center of the Plaza de Cibeles – a symbolic location for the city of Madrid – lies a circular fountain, whose lights illuminate the structure orange in the dark blue night sky. On the back of the new 21/22 Real Madrid home jersey lies a circular orange and blue patch, a call out to the structure and one that’s symbolic of how the city’s greatness and the players’ and fans’ camaraderie builds a circle of community in Madrid. This is their jersey – this is their grandeza.',
         img:'https://www.amstadion.com/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/r/m/rm-kjh02101-2_4.jpg',
         price: 90,
         qty:100
     },
     {
         name: 'Chelsea FC',
-        description: 'Home Chelsea FC Jersey.',
+        description: 'This kit made the world a cleaner place. The 21/22 Chelsea Home Jersey is made from 100% recycled bottles, protecting our environment, limiting waste, and making the Blues just a little bit green. As for its design – a 1960s West London art style known as Op-Art makes an appearance, using varied shapes, lines and spaces to create a dynamic, optically-fascinating jersey that’s full of movement, energy and vibrancy. That’s the spirit of West London you see waving across this new home shirt. That’s the Chelsea blue.',
         img:'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/b62dbf5d-11ee-4c63-995f-c80630157cd4/chelsea-fc-2021-22-stadium-home-mens-soccer-jersey-3BlHlg.png',
         price: 60,
         qty:100
     },
     {
         name: 'Inter Milan',
-        description: 'Home Inter Milan Jersey.',
+        description: 'Nike reach into the past to grab a storied Inter template to place a modern spin on with the 21/22 Inter Milan Third jersey. The base design comes courtesy of the club’s 89-91 away kit, with the color application not only looking to strike the modern, but also the inclusive – signaling through color the diverse and multicultural Milan, represented accurately with a multicolored spread.',
         img:'https://static1.cdn-subsidesports.com/2/media/catalog/product/cache/8c1d2c81075bec58441bdd78446b18bb/9/0/90b97ef5f592f1e2e7021ee0628040ce46bb2d4feca5dd08abb2cb7ecc1baa9f.jpeg',
         price: 100,
         qty:100
     },
     {
         name: 'Barcelona',
-        description: 'Away Barcelona Jersey.',
+        description: 'Barcelona – as they do – go where no club has gone before. The “purple pulse” color palette plays host to an iridescent club and Nike crest on the chest. Yes, a shiny, reflective badge application on a professional soccer kit. Nike didn’t hold back. The jersey is meant to be a kit for everyone, one driving unity and togetherness, because, after all – tots units fem forca.',
         img:'https://www.footballshirtculture.com/images/2021/barcelona_2021_2022_away_kit.jpg',
         price: 70,
         qty:100
     },
     {
         name: 'Manchester United',
-        description: 'Cristiano Ronaldo Home Manchester United Jersey.',
+        description: 'Barcelona – as they do – go where no club has gone before. The “purple pulse” color palette plays host to an iridescent club and Nike crest on the chest. Yes, a shiny, reflective badge application on a professional soccer kit. Nike didn’t hold back. The jersey is meant to be a kit for everyone, one driving unity and togetherness, because, after all – tots units fem forca.',
         img:'https://images.footballfanatics.com/manchester-united/manchester-united-cup-home-shirt-2021-22-with-ronaldo-7-printing_ss4_p-12093052+u-1ep8mddmd2rktpu3u302+v-149c3800d6d049d1a7f7e375c48d3860.jpg?_hv=1&w=600',
         price: 120,
         qty:100
     },
     {
         name: 'Juventus',
-        description: 'Home Juventus Jersey.',
+        description: 'Barcelona – as they do – go where no club has gone before. The “purple pulse” color palette plays host to an iridescent club and Nike crest on the chest. Yes, a shiny, reflective badge application on a professional soccer kit. Nike didn’t hold back. The jersey is meant to be a kit for everyone, one driving unity and togetherness, because, after all – tots units fem forca.',
         img:'https://store.juventus.com/data/store/product/4/47060/product.jpg',
         price: 50,
         qty:100
